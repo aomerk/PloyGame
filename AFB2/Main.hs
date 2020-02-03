04b53afcbad06c02519b8f06eb9be70fa60fe6e7
aomerk <aliomerkeser@gmail.com>
2020-02-03 13:38:49 +0300

    directions and legal moves of figures on board

--- AFB2/Main.hs
+++ AFB2/Main.hs
@@ -12,6 +12,7 @@ import Util
 
 import Data.Foldable
 import Data.Array
+import Data.Bits
 
 
 --- external signatures (NICHT AENDERN!)
@@ -31,7 +32,9 @@ data Column = Column { colIndex :: Int, element :: String, columnRowIndex :: Int
 figureList :: [Figure]
 
 
-data Figure = Figure {isWhite :: Bool, position :: Int, aimIndices :: [Int]} deriving(Show)
+data Figure = Figure {isWhite :: Bool, position :: Int, aimIndices :: [Int], maxDistance :: Int, isNull :: Bool, directions :: [Int] } deriving(Show)
+
+
 
 stringToArray s = listArray (0, length s - 1) s
 
@@ -55,17 +58,76 @@ enumerate = zip [0..]
 
 
 -- -- TODO turn board into list of strings for elements
-boardToElements :: String -> [Figure]
+boardToElements :: String -> [Column]
 
-boardToElements board = let rows = boardToRows board in let elems = concat([ rowToElements row  | row <- rows ]) in  [elemToFigure elem | elem <-elems]
+boardToElements board = let rows = boardToRows board in let elems = concat([ rowToElements row  | row <- rows ]) in elems
 
+boardToFigureList cols = [elemToFigure elem | elem <-cols]
 --
+
+figuresToField figures = [isWhite(elem) | elem <- figures]
 --
+
+directionToDistanceDictionary = [-9, -8, 1, 10 ,9 ,8 , -1, -10]
 
 -- -- TODO turn element string into a Figure Data
 elemToFigure :: Column -> Figure
-elemToFigure elem = Figure {isWhite = True, position = (colIndex(elem) + (columnRowIndex(elem) * 9)), aimIndices = [] }
+elemToFigure col =
+  let elem = element(col) :: String in
+  let (rest,owner, nullcheck) = figureOwner (elem) in
+  let idx = (colIndex(col) + (columnRowIndex(col) * 9))  in
+  let figureNum = readFigureNum rest in
+  Figure {
+  isWhite = owner, position = idx, aimIndices = [], directions = setDirections figureNum 0 [],
+  maxDistance = figureMaxDistance figureNum, isNull = nullcheck   }
 
+
+figureOwner :: String -> (String, Bool, Bool)
+figureOwner "" =  ("", False, True)
+figureOwner str = let owner = ((str !! 0) == 'w') in let rest = drop 1 str in (rest,owner, False)
+
+
+readFigureNum ::  String -> Int
+readFigureNum "" = 0
+readFigureNum rest =(if(rest == "" || rest == "," || rest == "/") then 0 else read rest :: Int)
+
+
+figureMaxDistance :: Int -> Int
+figureMaxDistance a =if(popCount a >= 4) then 1 else popCount a
+
+-- setDirections num 0 [] = setDirections (num, , [])
+
+-- input ===> 84 part of w84, empty array
+-- returns ====> array of possible directions
+setDirections :: Int-> Int -> [Int] -> [Int]
+
+setDirections num 8 directions = directions
+setDirections num n directions = if testBit num n
+  then setDirections num (n+1) (directions ++ [n])
+  else setDirections num (n+1) directions
+
+
+-- position , maxDistance
+setAimIndices :: Figure -> Int -> [Int] -> [Int]
+  -- setAimIndices position maxDistance directions = 3
+setAimIndices figure 0 aimIns = aimIns
+
+setAimIndices fig dist aimIns =
+  let positionInd = position(fig) in
+  let ins = [positionInd + (dist * a) |a <- directionToDistance (directions( fig))] in
+  let insideBoard = [x | x <- ins , x >= 0 , x <=80 ] in
+  let (col, row) = (positionInd `mod` 9, positionInd `div` 9 ) in
+  let aims = [aim |aim <- insideBoard, let aimCol = aim `mod` 9 , let aimRow = (aim `div` 9) :: Int,  abs (aimCol - col ) >= 0 , abs (aimRow - row) >= 0  ] in
+  setAimIndices fig (dist - 1) (aimIns ++ aims)
+
+
+directionToDistance dirs = [directionToDistanceDictionary!!x | x <- dirs]
+
+filterAims aims current  =
+  let (col, row) = (current `mod` 9, current `div` 9 ) in
+  [aim |  aim<- aims , aim >= 0, aim<=80,
+  let aimCol = aim `mod` 9 , let aimRow = (aim `div` 9) :: Int,
+  abs (aimCol - col ) >= 0 , abs (aimRow - row) >= 0     ]
 --
 -- -- TODO board string to figures list
 -- boardToFigureList :: String -> [String]
@@ -87,5 +149,8 @@ main :: IO ()
 main = do
   args <- getArgs
   let oneString = foldr (\x y -> if y == "" then x else x ++ " " ++ y) "" args
-  print((boardToElements (oneString) ) )
+  -- print((boardToElements (oneString) ) )
+  -- print(setDirections 84 0 [])
+  print((boardToFigureList (boardToElements (oneString) ) ) )
+
   putStrLn ( getMove oneString )
